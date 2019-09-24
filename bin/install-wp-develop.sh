02ab1b116a6d15307cf16ccda53d156cb3e70575
Parker Mathewson <theritesites@gmail.com>
2019-09-24 14:41:01 -0700

    second test

---
+++ bin/install-wp-develop.sh
@@ -0,0 +1,74 @@
+if [ $# -lt 3 ]; then
+        echo "usage: $0 <db-name> <db-user> <db-pass> [db-host] [wp-version] [skip-database-creation]"
+        exit 1
+fi
+
+DB_NAME=$1
+DB_USER=$2
+DB_PASS=$3
+DB_HOST=${4-localhost}
+WP_VERSION=${5-latest}
+SKIP_DB_CREATE=${6-false}
+
+download() {
+    if [ `which curl` ]; then
+        curl -s "$1" > "$2";
+    elif [ `which wget` ]; then
+        wget -nv -O "$2" "$1"
+    fi
+}
+
+set -ex
+
+install_wp() {
+
+	svn co https://develop.svn.wordpress.org/trunk/ web/
+        download https://raw.github.com/markoheijnen/wp-mysqli/master/db.php web/src/wp-content/db.php
+}
+
+install_db() {
+
+        # portable in-place argument for both GNU sed and Mac OSX sed
+        if [[ $(uname -s) == 'Darwin' ]]; then
+                local ioption='-i .bak'
+        else
+                local ioption='-i'
+        fi
+
+
+        if [ ! -f web/wp-tests-config.php ]; then
+		cp web/wp-tests-config-sample.php web/wp-tests-config.php
+		WP_CORE_DIR="web"
+                sed $ioption "s:dirname( __FILE__ ) . '/src/':'web/':" web/wp-tests-config.php
+                sed $ioption "s/youremptytestdbnamehere/$DB_NAME/" web/wp-tests-config.php
+                sed $ioption "s/yourusernamehere/$DB_USER/" web/wp-tests-config.php
+                sed $ioption "s/yourpasswordhere/$DB_PASS/" web/wp-tests-config.php
+                sed $ioption "s|localhost|${DB_HOST}|" web/wp-tests-config.php
+        fi
+
+        if [ ${SKIP_DB_CREATE} = "true" ]; then
+                return 0
+        fi
+
+        # parse DB_HOST for port or socket references
+        local PARTS=(${DB_HOST//\:/ })
+        local DB_HOSTNAME=${PARTS[0]};
+        local DB_SOCK_OR_PORT=${PARTS[1]};
+        local EXTRA=""
+
+        if ! [ -z $DB_HOSTNAME ] ; then
+                if [ $(echo $DB_SOCK_OR_PORT | grep -e '^[0-9]\{1,\}$') ]; then
+                        EXTRA=" --host=$DB_HOSTNAME --port=$DB_SOCK_OR_PORT --protocol=tcp"
+                elif ! [ -z $DB_SOCK_OR_PORT ] ; then
+                        EXTRA=" --socket=$DB_SOCK_OR_PORT"
+                elif ! [ -z $DB_HOSTNAME ] ; then
+                        EXTRA=" --host=$DB_HOSTNAME --protocol=tcp"
+                fi
+        fi
+
+        # create database
+        mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA
+}
+
+install_wp
+install_db
