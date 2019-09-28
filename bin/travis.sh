82bfc88babce740e8a5e80c237719341cae7999b
Parker Mathewson <theritesites@gmail.com>
2019-09-27 19:12:55 -0700

    added woocommerce install correctly

--- bin/travis.sh
+++ bin/travis.sh
@@ -11,8 +11,9 @@ if [ $1 == 'before' ]; then
 
 	# place a copy of woocommerce where the unit tests etc. expect it to be
 	mkdir -p "woocommerce"
-	#curl -L https://github.com/woocommerce/woocommerce/archive/3.7.0.tar.gz | tar --strip-components=1 -zx -C "woocommerce"
-	curl -L https://github.com/woocommerce/woocommerce.git | tar --strip-components=1 -zx -C "woocommerce"
+	# curl -L https://github.com/woocommerce/woocommerce/archive/3.7.0.tar.gz | tar --strip-components=1 -zx -C "woocommerce"
+	# curl -L https://github.com/woocommerce/woocommerce.git | tar -zx -C "woocommerce"
+	curl -L https://api.github.com/repos/woocommerce/woocommerce/tarball | tar -zx -C "woocommerce"
 
 	say "WooCommerce Installed"
 
